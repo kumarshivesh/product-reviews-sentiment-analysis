437f3f323f44d347b29d47847f25eedd5549e098
Kumar Shivesh <116376181+kumarshivesh@users.noreply.github.com>
2024-10-09 08:59:18 +0530

    Update ALLOWED_HOSTS for Render deployment

--- project/settings.py
+++ project/settings.py
@@ -124,7 +124,8 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1']
+#ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['product-reviews-sentiment-analysis.onrender.com', 'localhost', '127.0.0.1']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
